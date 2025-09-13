a548bfc3445f6d6918f4c2a4fb840b66c9b27c5f
hyunw00 <khwland090@gmail.com>
2025-09-13 19:42:48 +0900

    feat: tailwind 연결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import type { Metadata } from "next";
+import "@/styles/global.css";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -6,6 +7,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="theme-color" content="#111827" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
